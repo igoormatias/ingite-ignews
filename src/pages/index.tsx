99dd5341e5c2d625cedaf2d0bc4115dcba93b458
Igor Matias <igorhmatias@gmail.com>
2021-05-29 12:14:21 -0300

    feat: configurando fonte roboto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}> Hello</h1>
+    <h1 className={styles.title}>Hello</h1>
       
   )
 }
