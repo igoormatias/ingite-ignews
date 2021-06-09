8420b5ea8627527fab83d63ff09be5ac02dad0df
Igor Matias <igorhmatias@gmail.com>
2021-06-09 13:30:30 -0300

    feat: configurando chamada na api do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+
+import { stripe } from '../services/stripe'
+
+
 import { SubscribeButton } from '../components/SubscribeButton'
 import  styles  from './home.module.scss'
 
-
-export default function Home() {
+interface HomeProps {
+  product : {
+    priceId: string;
+    amount: number;
+  }
+}
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -15,14 +25,36 @@ export default function Home() {
           <h1>Novidades sobre o Universo <span>React</span></h1>
           <p>
             Tenha acesso a todas publicações <br />
-            <span> por apenas R$30,00 mensais</span>
+            <span> por apenas {product.amount} mensais</span>
 
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} /> 
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
       
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Iz6THCucdtOH45GTexrRNg9', {
+    expand: ['product']
+  }) 
+
+  const product = {
+    priceId: price.id,
+    amount:  new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency:'BRL'
+  }).format(price.unit_amount /100)
+  };
+
+  return { 
+    props: {
+      product,
+    }
+
+  }
+  
 }
